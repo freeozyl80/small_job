69806b697b1d5992d338656042cc49212c24dabe
zhangyunlu <zhangyunlu@baidu.com>
2017-03-22 21:35:39 +0800

    zhangyunlu

--- deepcopy.js
+++ deepcopy.js
@@ -1,3 +1,4 @@
+// 方法一
 function deepCopy(obj) {
     var newObj = {},
         srcQueue = [obj], srcVisitedQueue = [],
@@ -28,4 +29,38 @@ function deepCopy(obj) {
     }
 
     return newObj;
+}
+// 方法二
+var $ = (function(){
+  var types = 'Array Object String Date RegExp Function Boolean Number Null Undefined'.split(' ');
+  function type() {
+    return Object.prototype.toString.call(this).slice(8, -1);
+  }
+  for (var i = types.length; i--;) {
+     $['is' + types[i]] = (function (self) {
+        return function (elem) {
+           return type.call(elem) === self;
+        };
+    })(types[i]);
+  }
+  return $;
+})();//类型判断
+
+function copy(obj,deep){ 
+  if(obj === null || typeof obj !== "object"){ 
+     return obj; 
+  } 　　　　　
+  var name, target = $.isArray(obj) ? [] : {}, value; 
+  for(name in obj){ 
+     value = obj[name]; 
+     if(value === obj) {
+        continue;
+     }
+     if(deep && ($.isArray(value) || $.isObject(value))){
+       target[name] = copy(value,deep);
+     }else{
+       target[name] = value;
+     } 
+  } 
+  return target;
 }
